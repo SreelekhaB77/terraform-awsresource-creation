08a20b1da2d4a8b412e426c19ff8d904b213af9a
sreelekhab77 <133074494+SreelekhaB77@users.noreply.github.com>
2025-07-23 17:55:54 +0530

    Update main.tf

--- backend-setup/main.tf
+++ backend-setup/main.tf
@@ -1,1 +1,34 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = var.bucket_name
+  force_destroy = true
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "infra"
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = var.dynamodb_table
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "infra"
+  }
+}
 
