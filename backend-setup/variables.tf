5bde78ab5bc3b94afdf5e59f535f7931c4b175e4
sreelekhab77 <133074494+SreelekhaB77@users.noreply.github.com>
2025-07-23 17:56:25 +0530

    Update variables.tf

--- backend-setup/variables.tf
+++ backend-setup/variables.tf
@@ -1,1 +1,12 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  default = "terraform-state-bucket-yourname"  # Must be globally unique
+}
+
+variable "dynamodb_table" {
+  default = "terraform-lock-table"
+}
 
